d5687f139fbc95ce3a1983e5dddc13d7e04decd5
Issah Asiedu <issah.asiedu@amalitech.com>
2023-08-07 14:16:04 +0000

    add get instructors

--- Services/InstructorsService.cs
+++ Services/InstructorsService.cs
@@ -1,4 +1,6 @@
-﻿using UniversityDto;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using UniversityDto;
 using UniversityRestApi.Data;
 using UniversityShared.Models;
 
@@ -7,14 +9,31 @@ public class InstructorsService
 {
 
     private readonly Repository<Instructor> repository;
+    private readonly IMapper mapper;
 
-    public InstructorsService(Repository<Instructor> repository)
+    public InstructorsService(Repository<Instructor> repository, IMapper mapper)
     {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
-    public Task<InstructorPaginationData> GetInstructors(PaginationFilter filter)
+    public async Task<InstructorPaginationData> GetInstructors(PaginationFilter filter)
     {
-        throw new NotImplementedException();
+        static IQueryable<Instructor> query(IQueryable<Instructor> query)
+        {
+            return query.Include(i => i.Courses).ThenInclude(c => c.Enrollments).ThenInclude(e => e.Student)
+            .Include(i => i.OfficeAssignment);
+        }
+
+        var paginatedData = await repository.GetAll(filter, query);
+
+        var items = mapper.Map<List<InstructorDto>>(paginatedData.Items);
+
+        return new InstructorPaginationData(items)
+        {
+            Count = paginatedData.TotalItems,
+            CurrentIndex = paginatedData.CurrentIndex
+        };
+
     }
 }
